8b9b565b4b6026ff3a5859e06dda85baaa8eadbd
sanaullah <sanaullahhaifz@gmail.com>
2023-04-25 22:16:30 +0400

    added ga4 analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "@mui/material";
 
 import theme from "../theme";
 import Layout from "../components/Layout/Layout";
+import * as gtag from '../analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  console.log('gtaId:', process.env.NEXT_PUBLIC_GA_TRACKING_ID)
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
       <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,24 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <Main />
         <NextScript />
