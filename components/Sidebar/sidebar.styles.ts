8ad5f5c77072f26c9b54b66db98466ca1e3bf411
sanaullah <sanaullahhaifz@gmail.com>
2023-05-22 21:23:24 +0400

    added scrollbar styles

--- components/Sidebar/sidebar.styles.ts
+++ components/Sidebar/sidebar.styles.ts
@@ -2,6 +2,21 @@ export const styles = {
   drawer: {
     ".MuiDrawer-paperAnchorLeft": {
       backgroundColor: "primary.main",
+      overflow: "hidden",
+      overflowY: "auto",
+      scrollbarColor: "#616161 transparent",
+      scrollbarWidth: "thin",
+      "&::-webkit-scrollbar-track": {
+        backgroundColor: "transparent",
+      },
+      "&::-webkit-scrollbar": {
+        width: "6px",
+        height: "6px",
+        backgroundColor: "transparent",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "#616161",
+      },
     },
   },
   profileBtn: {
