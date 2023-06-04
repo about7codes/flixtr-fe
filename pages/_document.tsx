a7bc1e749e5fa3cccf47f2c69a255b16dec13d3f
sanaullah <sanaullahhaifz@gmail.com>
2023-06-04 10:39:38 +0400

    removed fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,10 +32,10 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          <script
+          {/* <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7007879471382156"
-          ></script>
+          ></script> */}
         </Head>
         <Main />
         <NextScript />
