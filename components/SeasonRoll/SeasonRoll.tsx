808d9226c203195626e246ce92da2da89ae757b8
Sanaullah <sanaullahhaifz@gmail.com>
2023-10-04 12:40:14 +0400

    removal of next image

--- components/SeasonRoll/SeasonRoll.tsx
+++ components/SeasonRoll/SeasonRoll.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { Box, Typography, Grid } from "@mui/material";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 import { ShowSeason } from "../../types/apiResponses";
 import { styles as classes } from "./seasonRoll.styles";
 import { blurData, formatImgSrc, toUrlFriendly } from "../../utils/utils";
@@ -25,12 +28,11 @@ const SeasonRoll = ({ seasonList, showId, showName }: SeasonRollProps) => {
               <Grid item sx={classes.seasonItem} key={index}>
                 <Link
                   style={{ WebkitTapHighlightColor: "transparent" }}
-                  href={`/tv/${showId}/${toUrlFriendly(showName)}/season/${
-                    season.season_number
-                  }`}
+                  href={`/tv/${showId}/${toUrlFriendly(showName)}/season/${season.season_number
+                    }`}
                 >
                   <Grid sx={classes.seasonImg}>
-                    <Image
+                    {/* <Image
                       fill
                       placeholder="blur"
                       blurDataURL={blurData}
@@ -44,7 +46,18 @@ const SeasonRoll = ({ seasonList, showId, showName }: SeasonRollProps) => {
                       )}
                       style={{ objectFit: "cover" }}
                       alt="backdrops"
+                    /> */}
+
+                    <LazyLoadImage
+                      src={formatImgSrc(
+                        "https://image.tmdb.org/t/p/w138_and_h175_face/",
+                        season.poster_path
+                      )}
+                      style={{ objectFit: "cover", objectPosition: "top", width: '100%', height: '100%' }}
+                      alt='seasons-cover'
+                      effect="blur"
                     />
+
                     <Box sx={classes.seasonBadge}>{season.season_number}</Box>
                   </Grid>
                   <Grid sx={classes.seasonNames}>
