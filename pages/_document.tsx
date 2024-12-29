368c85ed8c45ce4fb10c456908c20e9bb718feda
sanaullah <sanaullahhaifz@gmail.com>
2024-12-29 20:42:38 +0400

    added ms clarity analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,8 +31,6 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
           />
           <script
-            // history.scrollRestoration = 'manual';
-            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
@@ -42,6 +40,18 @@ export default class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
+            }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+            `,
             }}
           />
         </Head>
