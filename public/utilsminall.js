f6f7f627fee30f3724054b2862a9f8dfcca014b7
sanaullah <sanaullahhaifz@gmail.com>
2025-05-06 13:26:30 +0400

    START pa fixes

--- public/utilsminall.js
+++ public/utilsminall.js
@@ -1,47 +1,47 @@
 /*<![CDATA[/* */
 (function () {
-  var f = window,
-    j = "a576988a286a0e56adfc106121272073",
-    i = [
-      ["siteId", 93 * 525 - 738 + 5148980],
-      ["minBid", 0],
+  var p = window,
+    s = "a576988a286a0e56adfc106121272073",
+    w = [
+      ["siteId", 548 + 718 + 212 + 29 + 5195560],
+      ["minBid", 0.0015],
       ["popundersPerIP", "0"],
       ["delayBetween", 10],
       ["default", false],
       ["defaultPerDay", 0],
       ["topmostLayer", "auto"],
     ],
-    z = [
-      "d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vQ0JnRWwvUW1tYi9ybmlnaHRseS5taW4uanM=",
-      "ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQveGZhc3QtanNvbi1wYXRjaC5taW4uanM=",
+    c = [
+      "d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vTC9LRi9sbmlnaHRseS5taW4uanM=",
+      "ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQvbmZhc3QtanNvbi1wYXRjaC5taW4uanM=",
     ],
-    v = -1,
-    t,
-    q,
-    n = function () {
-      clearTimeout(q);
-      v++;
-      if (z[v] && !(1772442172000 < new Date().getTime() && 1 < v)) {
-        t = f.document.createElement("script");
-        t.type = "text/javascript";
-        t.async = !0;
-        var o = f.document.getElementsByTagName("script")[0];
-        t.src = "https://" + atob(z[v]);
-        t.crossOrigin = "anonymous";
-        t.onerror = n;
-        t.onload = function () {
-          clearTimeout(q);
-          f[j.slice(0, 16) + j.slice(0, 16)] || n();
+    g = -1,
+    v,
+    b,
+    e = function () {
+      clearTimeout(b);
+      g++;
+      if (c[g] && !(1772443569000 < new Date().getTime() && 1 < g)) {
+        v = p.document.createElement("script");
+        v.type = "text/javascript";
+        v.async = !0;
+        var l = p.document.getElementsByTagName("script")[0];
+        v.src = "https://" + atob(c[g]);
+        v.crossOrigin = "anonymous";
+        v.onerror = e;
+        v.onload = function () {
+          clearTimeout(b);
+          p[s.slice(0, 16) + s.slice(0, 16)] || e();
         };
-        q = setTimeout(n, 5e3);
-        o.parentNode.insertBefore(t, o);
+        b = setTimeout(e, 5e3);
+        l.parentNode.insertBefore(v, l);
       }
     };
-  if (!f[j]) {
+  if (!p[s]) {
     try {
-      Object.freeze((f[j] = i));
+      Object.freeze((p[s] = w));
     } catch (e) {}
-    n();
+    e();
   }
 })();
 /*]]>/* */
