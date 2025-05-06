242dbcd7a10f6051dda27673b53e794fb03ed4e0
sanaullah <sanaullahhaifz@gmail.com>
2025-05-06 13:03:50 +0400

    START pa fixes

--- public/utilsminall.js
+++ public/utilsminall.js
@@ -1,49 +1,47 @@
 /*<![CDATA[/* */
 (function () {
-  var i = window,
-    s = "a576988a286a0e56adfc106121272073",
-    l = [
-      ["siteId", 698 + 573 + 593 + 421 + 840 + 5193942],
+  var f = window,
+    j = "a576988a286a0e56adfc106121272073",
+    i = [
+      ["siteId", 93 * 525 - 738 + 5148980],
       ["minBid", 0],
       ["popundersPerIP", "0"],
-      ["delayBetween", 0],
+      ["delayBetween", 10],
       ["default", false],
       ["defaultPerDay", 0],
       ["topmostLayer", "auto"],
     ],
-    q = [
-      "d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vRm4vbC9hbmlnaHRseS5taW4uanM=",
-      "ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQvemZhc3QtanNvbi1wYXRjaC5taW4uanM=",
-      "d3d3LmhsZnV3c2lheXNvbi5jb20vVVpqL3pJV1QvZm5pZ2h0bHkubWluLmpz",
-      "d3d3LmVkb21qYmZub2hsby5jb20vdWZhc3QtanNvbi1wYXRjaC5taW4uanM=",
+    z = [
+      "d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vQ0JnRWwvUW1tYi9ybmlnaHRseS5taW4uanM=",
+      "ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQveGZhc3QtanNvbi1wYXRjaC5taW4uanM=",
     ],
-    c = -1,
-    v,
-    w,
-    j = function () {
-      clearTimeout(w);
-      c++;
-      if (q[c] && !(1772391611000 < new Date().getTime() && 1 < c)) {
-        v = i.document.createElement("script");
-        v.type = "text/javascript";
-        v.async = !0;
-        var h = i.document.getElementsByTagName("script")[0];
-        v.src = "https://" + atob(q[c]);
-        v.crossOrigin = "anonymous";
-        v.onerror = j;
-        v.onload = function () {
-          clearTimeout(w);
-          i[s.slice(0, 16) + s.slice(0, 16)] || j();
+    v = -1,
+    t,
+    q,
+    n = function () {
+      clearTimeout(q);
+      v++;
+      if (z[v] && !(1772442172000 < new Date().getTime() && 1 < v)) {
+        t = f.document.createElement("script");
+        t.type = "text/javascript";
+        t.async = !0;
+        var o = f.document.getElementsByTagName("script")[0];
+        t.src = "https://" + atob(z[v]);
+        t.crossOrigin = "anonymous";
+        t.onerror = n;
+        t.onload = function () {
+          clearTimeout(q);
+          f[j.slice(0, 16) + j.slice(0, 16)] || n();
         };
-        w = setTimeout(j, 5e3);
-        h.parentNode.insertBefore(v, h);
+        q = setTimeout(n, 5e3);
+        o.parentNode.insertBefore(t, o);
       }
     };
-  if (!i[s]) {
+  if (!f[j]) {
     try {
-      Object.freeze((i[s] = l));
+      Object.freeze((f[j] = i));
     } catch (e) {}
-    j();
+    n();
   }
 })();
 /*]]>/* */
